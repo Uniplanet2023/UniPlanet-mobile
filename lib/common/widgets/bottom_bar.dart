b6fe4d8a0d48d1938aba38be6b3d87f3245b4a78
Singularity2050 <uniplanet.info@gmail.com>
2024-03-13 22:41:10 -0400

    Update bottom_bar.dart

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uniplanet_mobile/bloc/account/account_bloc.dart';
@@ -36,6 +38,7 @@ class _BottomBarState extends State<BottomBar> {
   @override
   void initState() {
     super.initState();
+
     _controller = ScrollController();
     _controller!.addListener(() {
       if (_controller!.position.userScrollDirection ==
@@ -80,165 +83,166 @@ class _BottomBarState extends State<BottomBar> {
       const ChatList(),
       const AccountScreen(),
     ];
-
     return BlocListener<AccountBloc, AccountState>(
-        listener: (context, state) {
-          // TODO: implement listener
-          if (state is GotAccountInfoState) {
-            SocketService.instance.connect(context);
-          }
-        },
-        child: Scaffold(
-          body: Stack(children: [
-            pages[_page],
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 300),
-              bottom: _isVisible ? -20 : -120,
-              left: 0,
-              right: 0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.white,
-                    padding: _isVisible
-                        ? const EdgeInsets.fromLTRB(0, 8, 0, 10)
-                        : const EdgeInsets.fromLTRB(0, 8, 0, 40),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            height: 42,
-                            margin: const EdgeInsets.only(left: 15),
-                            child: Material(
-                              borderRadius: BorderRadius.circular(7),
-                              elevation: 1,
-                              child: InkWell(
-                                // Use InkWell to capture the tap event
-                                onTap: () =>
-                                    navigateToSearchScreen(), // Navigate to search screen on tap
-                                child: Container(
-                                  padding: const EdgeInsets.only(left: 12),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                        color: Colors.black38, width: 1),
-                                    borderRadius: BorderRadius.circular(7),
-                                  ),
-                                  child: const Row(
-                                    children: [
-                                      Icon(
-                                        Icons.search,
-                                        color: Colors.black54,
-                                        size: 23,
+      listener: (context, state) {
+        // TODO: implement listener
+        if (state is GotAccountInfoState) {
+          SocketService.instance.connect(context);
+        }
+      },
+      child: Scaffold(
+          body: Stack(
+        children: [
+          pages[_page],
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            bottom: _isVisible ? -15 : -120,
+            left: 0,
+            right: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.white,
+                  padding: _isVisible
+                      ? const EdgeInsets.fromLTRB(0, 8, 0, 10)
+                      : const EdgeInsets.fromLTRB(0, 8, 0, 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 42,
+                          margin: const EdgeInsets.only(left: 15),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(7),
+                            elevation: 1,
+                            child: InkWell(
+                              // Use InkWell to capture the tap event
+                              onTap: () =>
+                                  navigateToSearchScreen(), // Navigate to search screen on tap
+                              child: Container(
+                                padding: const EdgeInsets.only(left: 12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                      color: Colors.black38, width: 1),
+                                  borderRadius: BorderRadius.circular(7),
+                                ),
+                                child: const Row(
+                                  children: [
+                                    Icon(
+                                      Icons.search,
+                                      color: Colors.black54,
+                                      size: 23,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      'Search College Market',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 17,
                                       ),
-                                      SizedBox(width: 8),
-                                      Text(
-                                        'Search College Market',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 17,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        Container(
-                          color: Colors.white,
-                          height: 42,
-                          margin: const EdgeInsets.symmetric(horizontal: 10),
-                          child: const Icon(Icons.mic,
-                              color: Colors.black, size: 25),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        height: 42,
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        child: const Icon(Icons.mic,
+                            color: Colors.black, size: 25),
+                      ),
+                    ],
+                  ),
+                ),
+                BottomNavigationBar(
+                  currentIndex: _page,
+                  type: BottomNavigationBarType.fixed,
+                  selectedItemColor: GlobalVariables.selectedNavBarColor,
+                  unselectedItemColor: GlobalVariables.unselectedNavBarColor,
+                  selectedLabelStyle:
+                      const TextStyle(fontWeight: FontWeight.bold),
+                  backgroundColor: GlobalVariables.backgroundColor,
+                  enableFeedback: true,
+                  iconSize: 28,
+                  onTap: updatePage,
+                  items: <BottomNavigationBarItem>[
+                    const BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.home_outlined,
                         ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              BottomNavigationBar(
-                currentIndex: _page,
-                type: BottomNavigationBarType.fixed,
-                selectedItemColor: GlobalVariables.selectedNavBarColor,
-                unselectedItemColor: GlobalVariables.unselectedNavBarColor,
-                selectedLabelStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
-                backgroundColor: GlobalVariables.backgroundColor,
-                enableFeedback: true,
-                iconSize: 28,
-                onTap: updatePage,
-                items: <BottomNavigationBarItem>[
-                  const BottomNavigationBarItem(
+                        label: 'Home'),
+                    const BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.list,
+                        ),
+                        label: "Categories"),
+                    const BottomNavigationBarItem(
                       icon: Icon(
-                        Icons.home_outlined,
+                        Icons.add_box_outlined,
                       ),
-                      label: 'Home'),
-                  const BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.list,
-                      ),
-                      label: "Categories"),
-                  const BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.add_box_outlined,
+                      label: "sell",
                     ),
-                    label: "sell",
-                  ),
-                  BottomNavigationBarItem(
-                      icon: BlocBuilder<AccountBloc, AccountState>(
-                        builder: (context, state) {
-                          if (state.account.unreadMessage != 0) {
-                            return badges.Badge(
-                              badgeContent: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                constraints: const BoxConstraints(
-                                  minWidth: 10,
-                                  minHeight: 10,
-                                ),
-                                child: Text(
-                                  state.account.unreadMessage.toString(),
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12,
+                    BottomNavigationBarItem(
+                        icon: BlocBuilder<AccountBloc, AccountState>(
+                          builder: (context, state) {
+                            if (state.account.unreadMessage != 0) {
+                              return badges.Badge(
+                                badgeContent: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  constraints: const BoxConstraints(
+                                    minWidth: 10,
+                                    minHeight: 10,
+                                  ),
+                                  child: Text(
+                                    state.account.unreadMessage.toString(),
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              badgeStyle: const badges.BadgeStyle(
-                                elevation: 0,
-                                shape: badges.BadgeShape.circle,
-                              ),
-                              position: badges.BadgePosition.topEnd(
-                                  top: -12, end: -6),
-                              child: const Icon(
+                                badgeStyle: const badges.BadgeStyle(
+                                  elevation: 0,
+                                  shape: badges.BadgeShape.circle,
+                                ),
+                                position: badges.BadgePosition.topEnd(
+                                    top: -12, end: -6),
+                                child: const Icon(
+                                  Icons.chat_bubble_outline_sharp,
+                                ),
+                              );
+                            } else {
+                              return const Icon(
                                 Icons.chat_bubble_outline_sharp,
-                              ),
-                            );
-                          } else {
-                            return const Icon(
-                              Icons.chat_bubble_outline_sharp,
-                            );
-                          }
-                        },
-                      ),
-                      label: "Chat"),
-                  const BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.person_outline_sharp,
-                      ),
-                      label: 'Profile'),
-                ],
-              ),
-            )
-          ]),
-        ));
+                              );
+                            }
+                          },
+                        ),
+                        label: "Chat"),
+                    const BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.person_outline_sharp,
+                        ),
+                        label: 'Profile'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
