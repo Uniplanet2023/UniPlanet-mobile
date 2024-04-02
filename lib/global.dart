c23c4b0130a41f17e2566e6b53054777a4d8c405
Singularity2050 <uniplanet.info@gmail.com>
2024-04-02 02:57:24 -0400

    fix notification
    fix notification

--- lib/global.dart
+++ lib/global.dart
@@ -1,12 +1,8 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uniplanet_mobile/main.dart';
-import 'package:uniplanet_mobile/network/notification/firebase_api.dart';
-import 'package:uniplanet_mobile/main.dart';
-import 'package:uniplanet_mobile/network/notification/firebase_api.dart';
 import 'package:uniplanet_mobile/network/notification/firebase_options.dart';
 import 'package:uniplanet_mobile/network/api_def/dio_client.dart';
 
@@ -16,7 +12,7 @@ class Global {
     await DioClient.instance.initCookie();
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
-    await FirebaseApi().initNotification();
+    // await FirebaseApi().initNotification();
     FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
     await FirebaseMessaging.instance
         .setForegroundNotificationPresentationOptions(
@@ -28,7 +24,8 @@ class Global {
       // This is where you receive messages when the app is in the foreground.
       // If you want to prevent showing notifications in the foreground, simply do not display them here.
 
-      print("Received a message in the foreground: ${message.messageId}");
+      print("Received a message in the foreground: $message");
+      print(message.toString());
       // You can still handle data messages here, if needed.
     });
 
