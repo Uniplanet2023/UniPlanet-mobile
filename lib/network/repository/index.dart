a8a29c31789f91b582a390fe99fbfe910f9bc07b
Singularity2050 <uniplanet.info@gmail.com>
2024-04-02 01:23:34 -0400

    update
    update

--- lib/network/repository/index.dart
+++ lib/network/repository/index.dart
@@ -2,5 +2,4 @@ export 'account_repository/account_repo.dart';
 export 'auth_repository/auth_repo.dart';
 export 'chat_repository/chat_repo.dart';
 export 'product_repository/product_repo.dart';
-export 'user_repository/user_repo.dart';
 export '../api_def/dio_client.dart';
--- lib/network/repository/user_repository/user_repo.dart
+++
@@ -1,5 +0,0 @@
-import 'package:uniplanet_mobile/models/user_model.dart';
-
-abstract class UserRepositoryTest {
-  abstract Future<User> signUpUser;
-}
