3735ae3bd2cc6cf5211a7ed1ac4253f22e07c028
Singularity2050 <uniplanet.info@gmail.com>
2024-04-03 07:46:05 -0400

    android notification
    android notification

--- lib/network/notification/notification_service.dart
+++ lib/network/notification/notification_service.dart
@@ -14,7 +14,7 @@ class NotificationService {
   static Future<void> init() async {
     AwesomeNotifications().initialize(
         // set the icon to null if you want to use the default app icon
-        'resource://drawable/res_app_icon',
+        null,
         [
           NotificationChannel(
             channelGroupKey: 'basic_channel_group',
