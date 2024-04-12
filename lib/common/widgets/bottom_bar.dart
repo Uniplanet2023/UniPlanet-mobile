af1c6ff1dfd3c3ffb740a67f4e7218229dda8f2e
Singularity2050 <uniplanet.info@gmail.com>
2024-04-12 03:26:25 -0400

    category page update
    category page update

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -10,6 +10,7 @@ import 'package:uniplanet_mobile/constants/global_variables.dart';
 import 'package:uniplanet_mobile/features/account/screens/account-screen.dart';
 import 'package:uniplanet_mobile/features/add-product/screens/add_product_screen.dart';
 import 'package:uniplanet_mobile/features/category/screens/categories.dart';
+import 'package:uniplanet_mobile/features/category/screens/category_screen.dart';
 import 'package:uniplanet_mobile/features/chat/screens/chat_layout_screen.dart';
 import 'package:uniplanet_mobile/features/home/screens/home_screen.dart';
 import 'package:badges/badges.dart' as badges;
@@ -98,9 +99,9 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     List<Widget> pages = [
       HomeScreen(controller: _controller!),
-      const Padding(
-        padding: EdgeInsets.only(bottom: kBottomNavigationBarHeight + 40),
-        child: CategoriesPage(),
+      Padding(
+        padding: const EdgeInsets.only(bottom: kBottomNavigationBarHeight + 40),
+        child: CategoryPage(),
       ),
       const AddProductScreen(),
       const Padding(
---
+++ lib/features/category/screens/category_screen.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uniplanet_mobile/bloc/category/category_bloc.dart';
+import 'package:uniplanet_mobile/bloc/index.dart';
+import 'package:uniplanet_mobile/models/product.dart';
+
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<CategoryBloc>().add(const GetHotProductsEvent());
+  }
+
+  final List<Map<String, dynamic>> categories = [
+    {'name': 'Electronics & Appliances', 'image': 'assets/images/macbook.jpg'},
+    {'name': 'Furniture', 'image': 'assets/images/chair.jpg'},
+    {'name': 'Home & Garden', 'image': 'assets/images/fry pan.png'},
+    {'name': 'Game & Hobbies', 'image': 'assets/images/game.jpg'},
+    {'name': "Books & Music", 'image': 'assets/images/books.jpg'},
+    {'name': "Men's Fashion", 'image': 'assets/images/clothes.jpg'},
+    {'name': 'Health & Beauty', 'image': 'assets/images/toner.jpg'},
+    {'name': 'Sports & Outdoors', 'image': 'assets/images/gloves.jpg'},
+    {'name': 'Vehicles & Parts', 'image': 'assets/images/car.jpg'},
+    {'name': 'Other', 'image': 'assets/images/box.jpg'},
+  ];
+
+  final List<String> interests = [
+    'tickets',
+    'coat rack',
+    'freecycle',
+    'plates',
+    'toyota highlander',
+    'table top easel',
+    'moncler',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Categories',
+            style: GoogleFonts.roboto(
+              fontSize: 20.sp,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        leadingWidth: 200,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    '🔥Hot items in Stony Brook University',
+                    style: GoogleFonts.roboto(
+                        fontSize: 15.sp, fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(width: 8.w),
+                  Icon(Icons.arrow_forward_ios, size: 15.sp)
+                ],
+              ),
+            ),
+            BlocBuilder<CategoryBloc, CategoryState>(
+              builder: (context, state) {
+                if (state is LoadingCategoryState) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is LoadedCategoryState) {
+                  return Container(
+                    height: 150.h,
+                    padding: EdgeInsets.symmetric(horizontal: 8.w),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: state.categoryProducts.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return buildProductContent(
+                            state.categoryProducts[index]);
+                      },
+                    ),
+                  );
+                }
+                return const SizedBox();
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+              ),
+              child: Text('Browse by category',
+                  style: Theme.of(context).textTheme.titleLarge),
+            ),
+            // Search bar here
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  childAspectRatio: 9 / 10,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: categories.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ClipOval(
+                        child: Container(
+                          color: Colors.grey[200],
+                          width: 55.w,
+                          height: 55.h,
+                          child: Image.asset(categories[index]['image'],
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      Text(
+                        categories[index]['name'],
+                        style: const TextStyle(fontSize: 12),
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.visible,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Text('Based on your interests',
+                    style: Theme.of(context).textTheme.titleLarge)),
+            // Interests section here
+            // Interests section here
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Wrap(
+                spacing:
+                    8.w, // Space between individual chips on the same line.
+                children: interests
+                    .map((interest) => Chip(
+                          label: Text(interest),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildProductContent(Product product) => Container(
+      width: 140.w, // Set a fixed width for each card
+      padding:
+          EdgeInsets.symmetric(horizontal: 4.w), // Add some horizontal padding
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start
+
+        children: [
+          // Fixed size container for the image
+          Container(
+            height: 100.h, // Fixed height for the image
+            width: double.infinity, // Take the full width of the container
+            padding: EdgeInsets.all(8.w),
+            child: ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(8.w), // Rounded corners for the image
+              child: Image.network(
+                product.images[0], // Replace with your product image path
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Product name
+          Padding(
+            padding: EdgeInsets.all(4.w),
+            child: Text(
+              product.name, // Replace with your product name
+              style: TextStyle(
+                fontSize: 12.sp, // Use ScreenUtil for responsive font size
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ),
+          // Product price
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: 4.h), // Use ScreenUtil for responsive padding
+            child: Text(
+              '\$${product.price.toStringAsFixed(2)}', // Format the price to two decimal places
+              style: TextStyle(
+                fontSize: 10.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
