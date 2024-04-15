db5a9162eece910b3bfe95b6b8b51ee79fba1c9c
Natnael Bereda <natnael.bereda@stonybrook.edu>
2024-04-15 03:25:08 -0400

    user profile ui edit

--- lib/features/account/screens/user_profile.dart
+++ lib/features/account/screens/user_profile.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:uniplanet_mobile/constants/global_variables.dart';
+import 'package:lottie/lottie.dart';
 import 'package:uniplanet_mobile/features/account/screens/inventory_products_screen.dart';
 import 'package:uniplanet_mobile/features/account/screens/sold_products_screen.dart';
 import 'package:uniplanet_mobile/models/user_model.dart';
@@ -13,147 +13,141 @@ class UserProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange.shade50,
-      appBar: AppBar(
-        backgroundColor: Colors.orange.shade400,
-        title: const Text('User Profile'),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: const [0.2, 0.4, 0.6, 0.8, 1],
-                    colors: [
-                      Colors.orange.shade400,
-                      Colors.orange.shade300,
-                      Colors.orange.shade200,
-                      Colors.orange.shade100,
-                      Colors.orange.shade50,
+      body: Stack(children: [
+        SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Lottie.asset('assets/animations/background.json',
+              fit: BoxFit.cover),
+        ),
+        SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 90.h,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Hero(
+                        tag: 'user-pfp',
+                        child: CircleAvatar(
+                          radius: 60
+                              .w, // Assuming you have defined 'w' somewhere as a width factor
+                          backgroundImage:
+                              CachedNetworkImageProvider(user.profileImage!),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Text(
+                        user.name,
+                        style: TextStyle(
+                            fontSize: 0.09.sw,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        user.email,
+                        style:
+                            TextStyle(color: Colors.white, fontSize: 0.04.sw),
+                      ),
+                      Text(
+                        user.school,
+                        style: TextStyle(
+                            color: Colors.grey[600], fontSize: 0.04.sw),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Column(
+                            children: [
+                              // Text(
+                              //   'Listings',
+                              //   style: TextStyle(
+                              //       fontSize: 0.05.sw,
+                              //       fontWeight: FontWeight.bold),
+                              // ),
+                              // Text(
+                              //   '0',
+                              //   style: TextStyle(
+                              //     fontSize: 0.055.sw,
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 60.w,
+                          ),
+                          const Column(
+                            children: [
+                              // Text(
+                              //   'Sold',
+                              //   style: TextStyle(
+                              //       fontSize: 0.05.sw,
+                              //       fontWeight: FontWeight.bold),
+                              // ),
+                              // Text(
+                              //   '0',
+                              //   style: TextStyle(
+                              //     fontSize: 0.055.sw,
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
                     ],
                   ),
-                  color: GlobalVariables.secondaryColor,
-                  image: const DecorationImage(
-                    image: AssetImage('./assets/images/Logo.png'),
-                    opacity: 0.05,
-                  ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Hero(
-                      tag: 'user-pfp',
-                      child: CircleAvatar(
-                        radius: 60
-                            .w, // Assuming you have defined 'w' somewhere as a width factor
-                        backgroundImage:
-                            CachedNetworkImageProvider(user.profileImage!),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.h,
-                    ),
-                    Text(
-                      user.name,
-                      style: TextStyle(
-                          fontSize: 0.09.sw, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(user.email),
-                    Text(
-                      user.school,
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Column(
-                          children: [
-                            // Text(
-                            //   'Listings',
-                            //   style: TextStyle(
-                            //       fontSize: 0.05.sw,
-                            //       fontWeight: FontWeight.bold),
-                            // ),
-                            // Text(
-                            //   '0',
-                            //   style: TextStyle(
-                            //     fontSize: 0.055.sw,
-                            //   ),
-                            // ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 60.w,
-                        ),
-                        const Column(
-                          children: [
-                            // Text(
-                            //   'Sold',
-                            //   style: TextStyle(
-                            //       fontSize: 0.05.sw,
-                            //       fontWeight: FontWeight.bold),
-                            // ),
-                            // Text(
-                            //   '0',
-                            //   style: TextStyle(
-                            //     fontSize: 0.055.sw,
-                            //   ),
-                            // ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                  ],
+                SizedBox(
+                  height: 10.h,
                 ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              items(
-                  context,
-                  "Listings",
-                  "./assets/images/listings.jpeg",
-                  "Items currently available for sale by ${user.name}",
-                  InventoryProductsScreen(
-                    controller: ScrollController(),
-                    user: user,
-                  ),
-                  Icons.inventory_sharp),
-              SizedBox(
-                height: 5.h,
-              ),
-              items(
-                  context,
-                  "Sold",
-                  "./assets/images/sold.jpeg",
-                  "Previously sold items by ${user.name}",
-                  SoldProductsScreen(
-                    controller: ScrollController(),
-                    user: user,
-                  ),
-                  Icons.history),
-            ],
+                items(
+                    context,
+                    "Listings",
+                    "./assets/images/listings.jpeg",
+                    "Items currently available for sale by ${user.name}",
+                    InventoryProductsScreen(
+                      controller: ScrollController(),
+                      user: user,
+                    ),
+                    Icons.inventory_sharp),
+                SizedBox(
+                  height: 5.h,
+                ),
+                items(
+                    context,
+                    "Sold",
+                    "./assets/images/sold.jpeg",
+                    "Previously sold items by ${user.name}",
+                    SoldProductsScreen(
+                      controller: ScrollController(),
+                      user: user,
+                    ),
+                    Icons.history),
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
@@ -172,7 +166,7 @@ Widget items(BuildContext context, String name, String image,
         );
       },
       child: Card(
-        color: Colors.orange.shade50,
+        color: Colors.transparent,
         // Define the shape of the card
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
@@ -196,7 +190,7 @@ Widget items(BuildContext context, String name, String image,
                     child: Icon(
                       icon,
                       size: 90,
-                      color: Colors.black54,
+                      color: Colors.white,
                     ),
                   ),
                   // Add some spacing between the image and the text
@@ -214,6 +208,7 @@ Widget items(BuildContext context, String name, String image,
                           style: const TextStyle(
                             fontWeight: FontWeight.w800,
                             fontSize: 20,
+                            color: Colors.white,
                           ),
                         ),
                         // Add some spacing between the title and the subtitle
@@ -222,7 +217,7 @@ Widget items(BuildContext context, String name, String image,
                         Text(
                           "Check Out!",
                           style: TextStyle(
-                            color: Colors.grey[500],
+                            color: Colors.grey[300],
                           ),
                         ),
                         // Add some spacing between the subtitle and the text
@@ -232,7 +227,7 @@ Widget items(BuildContext context, String name, String image,
                           description,
                           maxLines: 2,
                           style:
-                              TextStyle(color: Colors.grey[700], fontSize: 16),
+                              TextStyle(color: Colors.grey[500], fontSize: 16),
                         ),
                       ],
                     ),
