b1dd712e7e0890e7217b2dbed861c23fd2c193d6
Singularity2050 <uniplanet.info@gmail.com>
2024-04-26 04:28:52 -0400

    Revert "fix apple requirement"
    This reverts commit 982658e93012e8a2646713051dc7f24267614416.

--- lib/features/product_details/screens/product_details_screen.dart
+++ lib/features/product_details/screens/product_details_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+
 import 'package:uniplanet/bloc/account/account_bloc.dart';
 import 'package:uniplanet/bloc/chat/chat_bloc.dart';
 import 'package:uniplanet/bloc/like/like_bloc.dart';
